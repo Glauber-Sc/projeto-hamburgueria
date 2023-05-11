35e3cacc14d6e2a70b3217211b071947a22f3c9b
Matheus Ramos <matheusdasilvaramos10@gmail.com>
2023-05-11 14:53:09 -0300

    Tabela de produtos adicionada

--- app/connections/database.py
+++ app/connections/database.py
@@ -4,6 +4,8 @@ db = orm.Database()
 db.bind(provider='postgres', user='postgres', password='1234', host='localhost', database='hamburgueria')
 
 class User(db.Entity):
+    _table_ = "users"
+
     id = orm.PrimaryKey(int, auto=True)
     name = orm.Required(str)
     email = orm.Required(str)
@@ -11,16 +13,24 @@ class User(db.Entity):
     admin = orm.Required(bool)
 
 
+class Category(db.Entity):
+    _table_ = "categories"
+
+    id = orm.PrimaryKey(int, auto=True)
+    name = orm.Required(str)
+    path = orm.Optional(str)
+    products = orm.Set("Product")
+
+
 class Product(db.Entity):
+    _table_ = "products"
+
     id = orm.PrimaryKey(int, auto=True)
     name = orm.Required(str)
     price = orm.Required(str)
     path = orm.Optional(str)
     offer = orm.Required(bool)
+    category = orm.Optional(Category)
 
-class Category(db.Entity):
-    id = orm.PrimaryKey(int, auto=True)
-    name = orm.Required(str)
-    path = orm.Optional(str)
     
 db.generate_mapping(create_tables=True)
